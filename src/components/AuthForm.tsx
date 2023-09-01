3762cf6f251013a2de2d26c556e457209ffe407a
DVNO <dvno@live.se>
2023-09-01 14:31:14 +0200

    user done

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import '../styles/AuthForm.css'
 
 function AuthForm() {
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isRegistered, setIsRegistered] = useState(true)
@@ -9,110 +10,125 @@ function AuthForm() {
   const [message, setMessage] = useState<string | null>(null)
   const [messageColor, setMessageColor] = useState<string | null>(null)
 
-  // This useEffect runs once when the component is mounted
   useEffect(() => {
     const storedUsers = localStorage.getItem('users')
     if (storedUsers) {
       const parsedUsers = JSON.parse(storedUsers)
-      // You can set the first user as the logged-in user here if needed
+      // Du kan sätta den första användaren som inloggad här om det behövs.
     }
   }, [])
+
+  const generateUserId = () => {
+    // Generera ett unikt användar-ID här (till exempel med hjälp av UUID eller någon annan metod).
+    // I detta exempel använder vi en enkel tidsstämpel för att skapa ett ID.
+    return Date.now().toString()
+  }
 
   const handleFormSubmit = (e: React.FormEvent) => {
     e.preventDefault()
 
-    // Clear any existing message
     setMessage(null)
     setMessageColor(null)
 
-    // Check if user is logging out
-    if (!email && !password && loggedInUser) {
-      // Do not remove user data from localStorage
+    if (!name && !email && !password && loggedInUser) {
       setLoggedInUser(null)
+      setName('')
       setEmail('')
       setPassword('')
-      setMessage('Logged out successfully.')
+      setMessage('Loggad ut.')
       setMessageColor('green')
       setTimeout(() => {
         setMessage(null)
         setMessageColor(null)
-      }, 3000) // Clear message after 3 seconds
+      }, 3000)
       return
     }
 
-    // Validate and save the user to localStorage
-    if (email && password) {
+    if (name && email && password) {
       const storedUsers = localStorage.getItem('users')
       const users = storedUsers ? JSON.parse(storedUsers) : []
 
       const userExists = users.some((user: any) => user.email === email)
 
       if (userExists) {
-        setMessage('User already exists. Login instead.')
+        setMessage('Användaren finns redan. Logga in istället.')
         setMessageColor('red')
         return
       }
 
-      const newUser = { email, password }
+      const userId = generateUserId() // Skapa ett unikt användar-ID
+      const newUser = { id: userId, name, email, password }
       users.push(newUser)
       localStorage.setItem('users', JSON.stringify(users))
 
-      setLoggedInUser(email)
-      setIsRegistered(true) // Reset to login form
+      setLoggedInUser(userId) // Sätt användarens ID som inloggad
+      setIsRegistered(true)
+      setName('')
       setEmail('')
       setPassword('')
-      setMessage('Registration successful.')
+      setMessage('Registreringen lyckades.')
       setMessageColor('green')
     } else {
-      setMessage('Please fill in both email and password.')
+      setMessage('Fyll i alla fält.')
       setMessageColor('red')
     }
 
     setTimeout(() => {
       setMessage(null)
       setMessageColor(null)
-    }, 3000) // Clear message after 3 seconds
+    }, 3000)
   }
 
   return (
     <div className="main_container">
       {loggedInUser ? (
         <div>
-          <h2>Welcome, {loggedInUser}!</h2>
-          <button onClick={handleFormSubmit}>Log Out</button>
+          <h2>Välkommen, användare {loggedInUser}!</h2>
+          <button onClick={handleFormSubmit}>Logga ut</button>
         </div>
       ) : (
         <div>
           <h2 style={{ color: messageColor || '' }}>{message}</h2>
-          <h2>{isRegistered ? 'Log in' : 'Register'}</h2>
+          <h2>{isRegistered ? 'Logga in' : 'Registrera'}</h2>
           <form onSubmit={handleFormSubmit}>
             <div>
-              <label>Email:</label>
+              <label htmlFor="Name">Namn:</label>
+              <input
+                type="text"
+                id="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="Email">E-post:</label>
               <input
                 type="email"
+                id="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
-              <label>Password:</label>
+              <label htmlFor="Password">Lösenord:</label>
               <input
                 type="password"
+                id="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div>
               <button type="submit">
-                {isRegistered ? 'Log in' : 'Register'}
+                {isRegistered ? 'Logga in' : 'Registrera'}
               </button>
             </div>
           </form>
           <div>
             <button onClick={() => setIsRegistered(!isRegistered)}>
               {isRegistered
-                ? "Don't have an account? Register here."
-                : 'Already have an account? Log in here.'}
+                ? 'Har du inget konto? Registrera här.'
+                : 'Har du redan ett konto? Logga in här.'}
             </button>
           </div>
         </div>
