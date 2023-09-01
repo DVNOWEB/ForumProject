a4f14b50c5f875338d170c3be416ef8db7b9075c
Gustav Clavin <gustav.clavin99@gmail.com>
2023-09-01 16:22:40 +0200

    Generating Manually added threads

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../src/styles/App.css';
+import '../src/styles/Thread.css';
+import '../src/styles/TreadListView.css';
 import AuthForm from './components/AuthForm'
-import './styles/App.css';
+
+import ThreadListView from './components/ThreadListView';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <AuthForm />
+    <div className="ThreadListContainer">
+      <ThreadListView />
     </div>
   );
 }
--- src/components/ThreadListView.tsx
+++ src/components/ThreadListView.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Thread from './Thread';
 
-const ThreadListView = () => {
-  return (
-    <div>ThreadListView</div>
-  )
+// Function to save a thread to localStorage
+function saveThreadToLocalStorage(threadData: Thread | QNAThread) {
+  try {
+    // Retrieve existing thread data from localStorage
+    const existingData = localStorage.getItem('threads');
+    const existingThreads = existingData ? JSON.parse(existingData) : [];
+
+    // Add the new thread to the existing threads
+    existingThreads.push(threadData);
+
+    // Save the updated threads to localStorage
+    localStorage.setItem('threads', JSON.stringify(existingThreads));
+  } catch (error) {
+    // Handle errors (e.g., localStorage is full, data is too large)
+    console.error('Error saving thread to localStorage:', error);
+  }
 }
 
-export default ThreadListView
+const newThread: Thread = {
+  id: 2,
+  title: 'En till tråd',
+  category: 'THREAD',
+  creationDate: 'idag',
+  description: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+  creator: {
+    id: 3,
+    name: 'Banan Mannen',
+    userName: 'Banana',
+  },
+};
+
+saveThreadToLocalStorage(newThread);
+//--------------------------------------------------------------------------------------------------
+function ThreadListView() {
+
+const [threads, setThreads] = useState<(Thread)[]>([])
+
+useEffect(() => {
+  const fetchData = async () => {
+    const threadData = localStorage.getItem('threads')
+    if(threadData) {
+      const parsedThreads = JSON.parse(threadData)
+      setThreads(parsedThreads)
+    }
+  }
+
+  fetchData()
+}, [])
+
+
+
+  return (
+    <div >
+      {threads.map((thread) => (
+        <Thread key={thread.id} thread={thread} />
+      ))}
+    </div>
+  );
+}
+
+export default ThreadListView;
