15a8192421ff4b8ca3f2b26c24224f87cff8753e
DVNO <dvno@live.se>
2023-09-03 12:50:11 +0200

    startin with styling the thread creation view

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import '../src/styles/App.css'
-// import './styles/App.css'
-// import AuthForm from './components/AuthForm'
-// import ThreadCreationView from './components/ThreadCreationView'
-
-// function App() {
-
-//   const handleCreateThread = (
-//     title: string,
-//     category: ThreadCategory,
-//     description: string
-//   ) => {
-//     console.log('Skapa tråd:', title, category, description)
-//   }
-
-//   return (
-//     <div className="App">
-//       <AuthForm />
-//       {/* Skicka med onCreateThread-funktionen som en prop */}
-//       <ThreadCreationView onCreateThread={handleCreateThread} />
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-// App.tsx
 import React, { useState } from 'react';
 import '../src/styles/App.css';
 import AuthForm from './components/AuthForm';
