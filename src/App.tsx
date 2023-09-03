67690c450b3a5a6035c3aa10ebee15f8bc8f8f64
DVNO <dvno@live.se>
2023-09-03 10:02:39 +0200

    Almost done... We are now going to commit the changes to the local repository.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import '../src/styles/App.css';
-import './styles/App.css';
+import React from 'react'
+import '../src/styles/App.css'
+import './styles/App.css'
 import AuthForm from './components/AuthForm'
 import ThreadCreationView from './components/ThreadCreationView'
 
-
-
 function App() {
+
+  const handleCreateThread = (
+    title: string,
+    category: ThreadCategory,
+    description: string
+  ) => {
+    console.log('Skapa tråd:', title, category, description)
+  }
+
   return (
     <div className="App">
       <AuthForm />
-      <ThreadCreationView />
+      {/* Skicka med onCreateThread-funktionen som en prop */}
+      <ThreadCreationView onCreateThread={handleCreateThread} />
     </div>
   )
 }
 
-export default App;
+export default App
